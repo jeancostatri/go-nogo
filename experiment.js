27f0c897f9e8ababc7d737bf8c36de974b0d1362
jeancostatri <100489968+jeancostatri@users.noreply.github.com>
2022-07-15 09:22:51 -0700

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -144,8 +144,10 @@ for (var i = 0; i < num_go_stim; i++) {
   })
 }
 
-var practice_trials = jsPsych.randomization.repeat(practice_stimuli, 5); 
-var test_trials = jsPsych.randomization.repeat(test_stimuli_block, 35);   
+//var practice_trials = jsPsych.randomization.repeat(practice_stimuli, 5); 
+//var test_trials = jsPsych.randomization.repeat(test_stimuli_block, 35);   
+var practice_trials = jsPsych.randomization.repeat(practice_stimuli, 1); 
+var test_trials = jsPsych.randomization.repeat(test_stimuli_block, 3);   
 
 
 
